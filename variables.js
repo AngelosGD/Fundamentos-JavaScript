7d5e1c84c71091cc4fa4fe5661f0815c48d5c3b9
AngelosDev <angelde9919@gmail.com>
2024-07-26 20:11:17 -0600

    first commit

---
+++ variables.js
@@ -0,0 +1,26 @@
+/*
+    TODO: Existen 3 tipos de variable en JavaScript:
+
+    ? 1-Variable var: esta variable ya no se usa mucho actualmente, ya que fue reemplzada por la palabra reservada let.
+    ? siendo estas 2 casi iguales, simplemente por cambios leves, variable que no tocaremos muy a fondo pero que en otro video hablare mas de ella.
+
+    ?? 2-Variable let: esta variable es de ambito local es decir, solo puede ser usada en su ambito de codigo o bloque de codigo.
+    ?? al igual que puede ser mutable a lo largo del codigo. y actualmente la usada a diferencia de var.
+
+    ?? 3-Variable const: al igual que la anterior esta variable es de ambito local es decir, solo puede ser usada en su ambito de codigo o bloque de codigo.
+    ?? al igual que puede ser immutable a lo largo del codigo o es decir no puede ser modificada dandole otro valor.
+*/
+
+
+/*function ponerNombre()   {
+    let nombre = "Angel";
+
+}
+
+function mandarLlamarnombre()   {
+    console.log(nombre)
+
+}*/
+
+const nombre = "Angel";
+console.log(nombre);
